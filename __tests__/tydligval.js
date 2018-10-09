8346c8f5675197fbcdc5e2d8138bce260623262d
Jozsef M Komjati <jozsef.komjati@blackcatsolutions.co.uk>
2018-10-09 20:23:51 +0100

    :spade: Add first set of snapshots :arrow_backward: No snapshots or any tests
    - Original Branch: feature/unit-testing-and-coverage

--- __tests__/tydligval.js
+++ __tests__/tydligval.js
@@ -1,1 +1,45 @@
-describe('tydligval', () => {});
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Picker from '../tydligval.js';
+
+describe('tydligval', () => {
+  const onSelect = jest.fn();
+  const items = [
+    {
+      id: 1,
+      value: 'test1',
+      label: 'Test 1',
+    },
+    {
+      id: 2,
+      value: 'test2',
+      label: 'Test 2',
+    },
+  ];
+
+  describe('with default interface', () => {
+    test('component render with no data', () => {
+      const shallow = new ShallowRenderer();
+      shallow.render(<Picker />);
+      const result = shallow.getRenderOutput();
+      expect(result).toMatchSnapshot();
+    });
+
+    test('component render with data', () => {
+      const shallow = new ShallowRenderer();
+      shallow.render(
+        <Picker
+          source={{
+            uri:
+              'https://img.freepik.com/free-vector/technology-background-with-gradient-colors_23-2147837710.jpg?size=338c&ext=jpg',
+          }}
+          key="picker-one"
+          onSelect={selected => this.onSelect(selected)}
+          items={items}
+        />,
+      );
+      const result = shallow.getRenderOutput();
+      expect(result).toMatchSnapshot();
+    });
+  });
+});
